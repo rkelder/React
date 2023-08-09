4e749abcfd3a69c847fea96af3015dc0f1da8ce2
rkelder <roderik.kelder@gmail.com>
2023-08-09 19:34:44 +0200

    Spelling

--- 00_ChallengeProjects/05_tipcalculator/src/Total.js
+++ 00_ChallengeProjects/05_tipcalculator/src/Total.js
@@ -5,7 +5,7 @@ export default function Total({ totalAmount, tip, tipAmount }) {
     <div className="row">
       <h3 className="header">Total Amount:</h3>
       <p className="header">
-        {totalAmount + tip} (${totalAmount} + ${tip} tip)
+        ${totalAmount + tip},- (${totalAmount} + ${tip} tip)
       </p>
     </div>
   )
