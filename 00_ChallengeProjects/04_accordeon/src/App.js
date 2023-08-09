9be8a30b2c6428f2454b6565aef76e29efa07c5b
rkelder <roderik.kelder@gmail.com>
2023-08-09 09:48:38 +0200

    Accordion V1.1

--- 00_ChallengeProjects/04_accordeon/src/App.js
+++ 00_ChallengeProjects/04_accordeon/src/App.js
@@ -25,20 +25,29 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null)
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   )
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false)
-
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen)
+    onOpen(isOpen ? null : num)
   }
 
   return (
@@ -46,7 +55,7 @@ function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   )
 }
